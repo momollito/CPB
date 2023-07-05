8a9a7d4076d898040314a1ea18fa6f16f81f1ce8
Mathis Rosset <rosset.mathis@gmail.com>
2023-07-05 17:21:42 +0200

    Initial Commit

---
+++ C'est pour Bibi/C_est_pour_BibiApp.swift
@@ -0,0 +1,17 @@
+//
+//  C_est_pour_BibiApp.swift
+//  C'est pour Bibi
+//
+//  Created by Mathis Rosset on 05/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct C_est_pour_BibiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
